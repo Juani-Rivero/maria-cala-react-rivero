20adf620cd59b0614ff11cc1158fd2d20e983f2d
Juan Ignacio Rivero <jr@adhoc.com.ar>
2021-09-08 19:57:08 -0300

    Cart + greeting

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,7 @@
+function Cart() {
+    return (
+        <a><i className="fas fa-shopping-cart"></i></a>
+    )
+};
+
+export default Cart;
---
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -0,0 +1,9 @@
+function ItemListContainer(props){
+    return(
+        <div className="greeting">
+            <h2>Bienvenido al e-commerce {props.name} !</h2>
+        </div>
+    )
+};
+
+export default ItemListContainer;
